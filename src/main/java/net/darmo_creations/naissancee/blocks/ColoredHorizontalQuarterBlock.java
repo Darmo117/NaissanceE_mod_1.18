2b15d036f8c1a78d25641edefa346c8751cefd78
Darmo117 <darmo117@gmail.com>
2022-08-03 20:02:06 +0200

    fix ColoredHorizontalQuarterBlock models

--- src/main/java/net/darmo_creations/naissancee/blocks/ColoredHorizontalQuarterBlock.java
+++ src/main/java/net/darmo_creations/naissancee/blocks/ColoredHorizontalQuarterBlock.java
@@ -1,11 +1,10 @@
 package net.darmo_creations.naissancee.blocks;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.sound.BlockSoundGroup;
 
-public class ColoredHorizontalQuarterBlock extends Block implements Colored {
+public class ColoredHorizontalQuarterBlock extends HorizontalQuarterBlock implements Colored {
   private final BlockColor color;
 
   public ColoredHorizontalQuarterBlock(final BlockColor color) {
